e6787fc5d0f90e957190d0e556aad073f7c80a4b
apple <apple@apples-MacBook-Pro.local>
2025-06-14 14:32:56 +0530

    update get ergst typo

--- src/test/java/userManagement/getErgst.java
+++ src/test/java/userManagement/getErgst.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class getErgst {
-    git a
     @Test(groups="SmokeSuite")
     public void myFailingTest() {
         System.out.println("Executing test...");
